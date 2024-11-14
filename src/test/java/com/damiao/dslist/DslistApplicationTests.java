74a03f0fbf360c560bbad27947b84e1a015a3cde
Henrique Damião <henriquedamiao1709@gmail.com>
2024-11-14 00:08:25 -0300

    create project

---
+++ src/test/java/com/damiao/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiao.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
