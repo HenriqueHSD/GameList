b57be6033cef0826ff64ee1c2d162ed0a5d7eef6
Henrique Damião <henriquedamiao1709@gmail.com>
2024-11-14 23:10:12 -0300

    GameList

---
+++ src/main/java/com/damiao/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.damiao.dslist.dto;
+
+import com.damiao.dslist.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO(){
+
+    }
+
+    public GameListDTO(GameList entity){
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
