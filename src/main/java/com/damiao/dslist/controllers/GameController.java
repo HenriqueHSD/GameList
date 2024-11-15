31d3df7d87d714a2cc9e491c921965d16060f292
Henrique Damião <henriquedamiao1709@gmail.com>
2024-11-14 22:58:12 -0300

    GameDTO, buscar por id

--- src/main/java/com/damiao/dslist/controllers/GameController.java
+++ src/main/java/com/damiao/dslist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.damiao.dslist.controllers;
 
+import com.damiao.dslist.dto.GameDTO;
 import com.damiao.dslist.dto.GameMinDTO;
 import com.damiao.dslist.entities.Game;
 import com.damiao.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,13 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+
+    }
 
     @GetMapping
     public List<GameMinDTO> fingAll(){
