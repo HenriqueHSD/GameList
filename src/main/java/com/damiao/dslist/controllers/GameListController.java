25abb5be95b5d2681c22626ec27ab4bf68fd9e94
Henrique Damião <henriquedamiao1709@gmail.com>
2024-11-19 04:03:57 -0300

    Move games na lista

--- src/main/java/com/damiao/dslist/controllers/GameListController.java
+++ src/main/java/com/damiao/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.damiao.dslist.controllers;
 
 import com.damiao.dslist.dto.GameListDTO;
 import com.damiao.dslist.dto.GameMinDTO;
+import com.damiao.dslist.dto.ReplacementDTO;
 import com.damiao.dslist.services.GameListService;
 import com.damiao.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +33,12 @@ public class GameListController {
         return result;
 
     }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+
+    }
+
 
 }
